fb62096294c577f22a6cfd712c2a5f0cb8a1e16f
PFreedude <168471033+PFreedude@users.noreply.github.com>
2024-08-06 16:59:22 -0700

    Update simple.py

--- lib/python/examples/simple.py
+++ lib/python/examples/simple.py
@@ -24,43 +24,42 @@ state of the servo to the console.
 import asyncio
 import math
 import moteus
+import time
+import argparse
 
-async def main():
+async def main(pos, id=1):
     # By default, Controller connects to id 1, and picks an arbitrary
     # CAN-FD transport, prefering an attached fdcanusb if available.
-    c = moteus.Controller()
+    c = moteus.Controller(id=id)
+    c.make_stay_within(maximum_torque=0.1)
+    time.sleep(1)
+    state = await c.set_position(position=pos, query=True)
+    time.sleep(0.1)
+    print(state)
+    print()
+    time.sleep(1)
+    await  c.set_stop()
 
-    # In case the controller had faulted previously, at the start of
-    # this script we send the stop command in order to clear it.
-    await c.set_stop()
+    time.sleep(2)    
+    c2 = moteus.Controller(id=2)
+    c2.make_stay_within(maximum_torque=0.1)
+    time.sleep(1)
+    print("about to test positioning!")
+    state = await c2.set_position(position=pos, query=True)
+    time.sleep(0.1)
+    print(state)
+    print()
+    time.sleep(1)
+    await  c2.set_stop()
+    time.sleep(2)
+    
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Control Moteus motor positions")
+    parser.add_argument("-p", "--position", type=float, default=0.0)  
+    parser.add_argument("-d", "--id", type=int, default=1)  
 
-    while True:
-        # `set_position` accepts an optional keyword argument for each
-        # possible position mode register as described in the moteus
-        # reference manual.  If a given register is omitted, then that
-        # register is omitted from the command itself, with semantics
-        # as described in the reference manual.
-        #
-        # The return type of 'set_position' is a moteus.Result type.
-        # It has a __repr__ method, and has a 'values' field which can
-        # be used to examine individual result registers.
-        state = await c.set_position(position=math.nan, query=True)
-
-        # Print out everything.
-        print(state)
-
-        # Print out just the position register.
-        print("Position:", state.values[moteus.Register.POSITION])
-
-        # And a blank line so we can separate one iteration from the
-        # next.
-        print()
-
-        # Wait 20ms between iterations.  By default, when commanded
-        # over CAN, there is a watchdog which requires commands to be
-        # sent at least every 100ms or the controller will enter a
-        # latched fault state.
-        await asyncio.sleep(0.02)
-
-if __name__ == '__main__':
-    asyncio.run(main())
+    args = parser.parse_args()
+    pos = args.position
+    id = args.id
+    asyncio.run(main(pos, id))
